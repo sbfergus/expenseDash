4d88edec515dc28c9959d73c4e3d5d767fd354f1
Scott <sbferguson17@gmail.com>
2022-09-19 13:55:18 -0600

    enabled user inserted expenses to add to list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
+import { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 209.49,
+    date: new Date(2022, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.99,
+    date: new Date(2022, 4, 4),
+  },
+  {
+    id: "e3",
+    title: "Utilities",
+    amount: 35.79,
+    date: new Date(2022, 7, 15),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 209.49,
-      date: new Date(2022, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.99,
-      date: new Date(2022, 4, 4),
-    },
-    {
-      id: "e3",
-      title: "Utilities",
-      amount: 35.79,
-      date: new Date(2022, 7, 15),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("in App js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
