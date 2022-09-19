4d68afdaeed5fc266073996d3d0e9109e0dbdc14
Scott <sbferguson17@gmail.com>
2022-09-19 13:46:59 -0600

    dynamically render expense items

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,22 +14,16 @@ function Expenses(props) {
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={filterYear} onYearSelected={saveFilterYear} />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
+        <ExpensesFilter selected={filterYear} onYearSelected={saveFilterYear} />{" "}
+        {props.expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
       </Card>
     </div>
   );
